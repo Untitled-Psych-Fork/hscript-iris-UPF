ff756f7cb64b0780ecf6c172b8208b29bc8a3ca0
kangel <132186966+kangeluwu@users.noreply.github.com>
2025-08-15 12:18:07 +0800

    我自裁
    This reverts commit dc8f0f89533a6bfabfe1a2aaa4892df833cfa451.

--- tests/source/Main.hx
+++ tests/source/Main.hx
@@ -11,6 +11,7 @@ import crowplexus.iris.Iris;
 
 @:build(macros.TestingMacro.build())
 class Main {
+	@:noTest
 	public static function test_hello_world() {
 		var script: HScript = new HScript("hello_world");
 		script.execute();
@@ -19,6 +20,7 @@ class Main {
 			script.call("mathTen");
 	}
 
+	@:noTest
 	public static function test_static_variables() {
 		var script1: HScript = new HScript("static_variables_1");
 		script1.execute();
@@ -28,12 +30,14 @@ class Main {
 	}
 
 	@:testName("typedef & name")
+	@:noTest
 	public static function test_typedef_enum() {
 		var script: HScript = new HScript("typedef_enum", true, false, false);
 		script.execute();
 	}
 
 	@:testName("regex & interpolation")
+	@:noTest
 	public static function test_regex_interpolation() {
 		var script: HScript = new HScript("test_regex_interpolation", false, false, true);
 		script.set("interpolation_player", "Beihu235");
@@ -41,6 +45,7 @@ class Main {
 	}
 
 	@:testName("import shared variables")
+	@:noTest
 	public static function test_import_shared() {
 		var script1: HScript = new HScript("shared/transmitter", false, false, false, "shared");
 		script1.execute();
@@ -50,11 +55,13 @@ class Main {
 	}
 
 	@:testName("is type")
+	@:noTest
 	public static function test_isType() {
 		var script: HScript = new HScript("isType");
 		script.execute();
 	}
 
+	@:noTest
 	public static function test_class_samples() {
 		var script: HScript = new HScript("class_samples", false, true, false);
 		script.execute();
@@ -64,6 +71,7 @@ class Main {
 		}
 	}
 
+	@:noTest
 	public static function test_switch_case() {
 		var script: HScript = new HScript("switch_case", true);
 		script.set("EnumSample", samples.enums.EnumSample);
