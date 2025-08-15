1fbb9bcb9456e021c536f88f50765ec5d09ddf9b
kangel <132186966+kangeluwu@users.noreply.github.com>
2025-08-15 12:28:18 +0800

    Revert "s"
    This reverts commit 331a5e1839208489ddc4d164f742010a922a7e12.

--- tests/source/samples/BaseSample.hx
+++ tests/source/samples/BaseSample.hx
@@ -1,29 +1,28 @@
 package samples;
 
 class BaseSample<T> {
-	public var active: Bool;
-	public var exists: Bool;
+	public var active:Bool;
+	public var exists:Bool;
 
-	public var output(default, null): T;
-
-	private var input: Null<T>;
+	public var output(default, null):T;
+	private var input:Null<T>;
 
 	public function new() {
 		active = exists = true;
 	}
 
-	public function inputContent(input: T): Void {
-		if (this.active)
+	public function inputContent(input:T):Void {
+		if(this.active)
 			this.input = input;
 	}
 
-	public function working(): Void {}
+	public function working():Void {}
 
-	public function destroy(): Void {
+	public function destroy():Void {
 		active = false;
 	}
 
-	public function toString(): String {
+	public function toString():String {
 		return '(active: ${this.active} | exists: ${this.exists})';
 	}
 }
